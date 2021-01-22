5fbd44726b79d938aa4f2f8ac99d2c68aa17697c
Joshua Raneri <raneri.josh@gmail.com>
2021-01-22 10:02:09 -0500

    cleaning up my mess 2.0

--- Backend/P2_Main/Program.cs
+++ Backend/P2_Main/Program.cs
@@ -18,11 +18,6 @@ namespace P2_Main
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
