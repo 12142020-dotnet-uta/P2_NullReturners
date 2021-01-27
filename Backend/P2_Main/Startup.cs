dd2b24d8d411eaa0d165b19a91da474a1629ac49
Daniel Baker <dbaker731@gmail.com>
2021-01-27 00:00:25 -0600

    login post works

--- Backend/P2_Main/Startup.cs
+++ Backend/P2_Main/Startup.cs
@@ -71,6 +71,12 @@ namespace P2_Main
                     };
                 });
 
+            // dont thing i need this
+            services.AddControllers()
+                 .AddNewtonsoftJson(options =>
+                 {
+                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
