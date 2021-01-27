31f2f9f2f5232ebb152859922198e28c5daeb21c
Daniel Baker <dbaker731@gmail.com>
2021-01-26 22:53:12 -0600

    fixing a backend bug

--- Backend/P2_Main/Program.cs
+++ Backend/P2_Main/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace P2_Main
 {
@@ -19,13 +18,6 @@ namespace P2_Main
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
