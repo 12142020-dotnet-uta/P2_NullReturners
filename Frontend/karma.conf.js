fb78fce0db656a8e1b8e79b15786c86359c2d190
Joshua Raneri <raneri.josh@gmail.com>
2021-01-29 14:02:56 -0500

    losing my mind

--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       jasmine: {
@@ -34,7 +35,10 @@ module.exports = function (config) {
       ],
       fixWebPackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: './Frontend/coverage/junit'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
