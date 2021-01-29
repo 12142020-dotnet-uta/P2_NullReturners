160e2b22bc40021ee75a340ad73c2f6416863275
Travis Martin <Travis Martin@DESKTOP-GSTL8RO>
2021-01-29 06:25:21 -0500

    wrote some frontend tests

--- Frontend/src/app/nav/nav.component.spec.ts
+++ Frontend/src/app/nav/nav.component.spec.ts
@@ -3,13 +3,21 @@ import { FormsModule } from '@angular/forms';
 
 import { NavComponent } from './nav.component';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { By } from '@angular/platform-browser';
+import {Location} from '@angular/common';
+import { Component } from '@angular/core';
+
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormsModule],
+      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule
+        .withRoutes([{path: '', component: DummyComponent}, {path: 'plays', component: DummyComponent}])],
       declarations: [ NavComponent ]
     })
     .compileComponents();
@@ -24,4 +32,62 @@ describe('NavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show unordered list items', () => {
+    const unorderedList = fixture.debugElement.queryAll(By.css('ul'));
+    expect(unorderedList.length).toBe(1);
+  });
+
+  it('should show unordered list items', () => {
+    const listItems = fixture.debugElement.queryAll(By.css('li'));
+    expect(listItems.length).toBe(7);
+  });
+
+  it('should navigate to home page', () => {
+    const location = TestBed.inject(Location)
+    expect(location.path()).toBe('');
+  });
+
+  it('should submit username and password to login', () => {
+    const listOfInputs = fixture.debugElement.queryAll(By.css('input'));
+    const userName: HTMLInputElement = listOfInputs[0].nativeElement;
+    const password: HTMLInputElement = listOfInputs[1].nativeElement;
+    fixture.detectChanges();
+
+    userName.value = "travis";
+    password.value = "travis123";
+
+    userName.dispatchEvent(new Event('input'));
+    password.dispatchEvent(new Event('input'));
+
+    expect(userName.value).toBe('travis');
+    expect(password.value).toBe('travis123');
+
+    spyOn(component, "login");
+    const allButtons = fixture.debugElement.queryAll(By.css('button'));
+    const submit: HTMLLinkElement = allButtons[1].nativeElement;
+    submit.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.login()).toHaveBeenCalled();
+    }); 
+  });
+
+  it('should call logout', () => {
+    spyOn(component, "logout");
+    const listOfLinks = fixture.debugElement.queryAll(By.css('a'));
+    const logout: HTMLInputElement = listOfLinks[7].nativeElement;
+    logout.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.logout()).toHaveBeenCalled();
+    });
+  });
+
 });
+
+
+@Component({template: ''})
+class DummyComponent {
+
+}
--- Frontend/src/app/players/create-player/create-player.component.spec.ts
+++ Frontend/src/app/players/create-player/create-player.component.spec.ts
@@ -2,15 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreatePlayerComponent } from './create-player.component';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import {Location} from '@angular/common';
+import { Router } from '@angular/router';
+
 describe('CreatePlayerComponent', () => {
   let component: CreatePlayerComponent;
   let fixture: ComponentFixture<CreatePlayerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule.withRoutes([{path: 'players', component: DummyComponent}])],
       declarations: [ CreatePlayerComponent ]
     })
-    .compileComponents();
+    .compileComponents(); 
   });
 
   beforeEach(() => {
@@ -22,4 +32,58 @@ describe('CreatePlayerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an h3 tag', () => {
+    const fixture = TestBed.createComponent(CreatePlayerComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('div').textContent).toContain('Add New User');
+  });
+
+  it('should have form inputs', () => {
+    const allInputs = fixture.debugElement.queryAll(By.css('input'));
+    const userNameInput: HTMLInputElement = allInputs[0].nativeElement;
+    expect(userNameInput.name).toBe('Username');
+    const passwordInput: HTMLInputElement = allInputs[1].nativeElement;
+    expect(passwordInput.name).toBe('Password');
+    const fullNameInput: HTMLInputElement = allInputs[2].nativeElement;
+    expect(fullNameInput.name).toBe('FullName');
+    const phoneNumberInput: HTMLInputElement = allInputs[3].nativeElement;
+    expect(phoneNumberInput.name).toBe('PhoneNumber');
+    const emailInput: HTMLInputElement = allInputs[4].nativeElement;
+    expect(emailInput.name).toBe('Email');
+  });
+
+  it('should have form submit button', () => {
+    const allButtons = fixture.debugElement.queryAll(By.css('button'));
+    const formSubmitButton: HTMLInputElement = allButtons[0].nativeElement;
+    expect(formSubmitButton.textContent).toBe('Create');
+  });
+
+  it('should navigate to create player', () => {
+    const location = TestBed.inject(Location)
+    expect(location.path()).toBe('');
+  });
+
+  it('should navigate to players when clicking on link', () => {
+    // const router = TestBed.inject(Router)
+    // spyOn(router, 'navigateByUrl');
+    const location = TestBed.inject(Location)
+    const allLinks = fixture.debugElement.queryAll(By.css('a'));
+    const playerLink: HTMLLinkElement = allLinks[0].nativeElement;
+    playerLink.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/players');
+    }); 
+    // fixture.whenStable().then(() => {
+    //   expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['/players']), 
+    //   { skipLocationChange: false, replaceUrl: false});
+    // });    
+  });
 });
+
+@Component({template: ''})
+class DummyComponent {
+
+}
