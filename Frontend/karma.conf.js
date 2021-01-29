1bb6354bb544fc0c3332e5adb77ec8e84e6d6c0b
Joshua Raneri <raneri.josh@gmail.com>
2021-01-29 14:31:28 -0500

    inching closer

--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
+        { type: 'lcov' },
         { type: 'text-summary' },
         { type: 'cobertura' }
       ],
