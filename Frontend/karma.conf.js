00c719e6d198a0e5a30de2af21e986798497a658
Joshua Raneri <raneri.josh@gmail.com>
2021-01-30 12:04:15 -0500

    .

--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
         { type: 'cobertura' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
